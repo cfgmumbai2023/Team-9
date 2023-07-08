9ca3061d141320a843edd64308a4dc1a3abce6c4
Nikit <nikit20022@gmail.com>
2023-07-09 02:21:17 +0530

    Nikit init code

---
+++ src/components/Customize.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Customize() {
+  return (
+    <div>
+      <section className="Customize">
+        <div className="container-fluid bg-success-subtle">
+          <div className="row justify-content-center align-content-center py-5">
+            <div className="col-md-4">
+              <h1>A Customized Approach</h1>
+            </div>
+            <div className="col-md-4">
+              <p>
+                Urmi collaborates with community-based schools and BMC special
+                schools to create tailored education programs for children with
+                disabilities, focusing on their holistic development.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Customize;
