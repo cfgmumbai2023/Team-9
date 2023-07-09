96fc04b66fb7070d146656783c17426596e5dee5
Nikit <nikit20022@gmail.com>
2023-07-09 05:45:25 +0530

    added faq

---
+++ src/components/Faq.js
@@ -0,0 +1,104 @@
+import React from "react";
+
+function Faq() {
+  return (
+    <div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <h2 className="text-center py-5">Frequently Asked Questions</h2>
+          </div>
+
+          <div className="col-md-12">
+            <div className="accordion" id="accordionExample">
+              <div className="accordion-item">
+                <h2 className="accordion-header">
+                  <button
+                    className="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    How does Urmi assess the students?
+                  </button>
+                </h2>
+                <div
+                  id="collapseOne"
+                  className="accordion-collapse collapse show"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">
+                    <p>
+                      Urmi assesses the students based on parameters like
+                      disability type, objectives, etc., provided by the Indian
+                      Government.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseTwo"
+                    aria-expanded="false"
+                    aria-controls="collapseTwo"
+                  >
+                    How are the curriculums modified?
+                  </button>
+                </h2>
+                <div
+                  id="collapseTwo"
+                  className="accordion-collapse collapse"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">
+                    <p>
+                      We analyze the assessment parameters regularly and tweak
+                      the curriculums to best fit the students' needs.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseThree"
+                    aria-expanded="false"
+                    aria-controls="collapseThree"
+                  >
+                    What disabilities do the children in the program have?
+                  </button>
+                </h2>
+                <div
+                  id="collapseThree"
+                  className="accordion-collapse collapse"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">
+                    <p>
+                      The Urmi Foundation works with children who have a range
+                      of developmental disabilities, including but not limited
+                      to autism spectrum disorders, Down syndrome, intellectual
+                      disabilities, and learning disabilities. We strive to
+                      provide inclusive support for children with diverse needs.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Faq;
