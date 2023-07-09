55cbccbb620bdb492cf0baf2741b46c445598047
Adarsh Rawat <43747987+adarsh-rawat@users.noreply.github.com>
2023-07-09 07:43:57 +0530

    Update App.js to include route for reports

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
+import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 import Customize from "./components/Customize";
-import Faq from "./components/Faq";
-import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Custom from './components/Custom.js'
 
 function App() {
-  
-  return (
-    <div>
+  const Home = () => {
+    return <div>
       <Navbar></Navbar>
       <Customize></Customize>
       <Card></Card>
-      <Faq></Faq>
-      <Footer></Footer>
     </div>
+  }
+  return (
+    <BrowserRouter>
+
+    <div className="page">
+        <Routes>
+          <Route element={<Home />} path='/'/>
+          <Route element={<Custom />} path='/custom-reports'/>
+        </Routes>
+    </div>
+    </BrowserRouter>
+    
   );
 }
 
